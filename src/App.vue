fa917822916a8758fe0a1c0ffd7906cd52580c60
Stephan Protsack <stephan@speedandfunction.com>
2023-09-18 12:19:35 +0200

    Add default empty setup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { NLayout, NLayoutContent, NConfigProvider, type GlobalThemeOverrides } from 'naive-ui'
+import { darkTheme } from 'naive-ui'
+
+const primaryColor = '#4263eb'
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    fontSize: '18px',
+    primaryColor: primaryColor,
+    primaryColorHover: primaryColor,
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <n-config-provider :theme="darkTheme" :theme-overrides="themeOverrides">
+    <n-layout class="wme-app-container">
+      <n-layout-content></n-layout-content>
+    </n-layout>
+  </n-config-provider>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style >
+.wme-app-container {
+  min-height: 100vh !important;
 }
 </style>
