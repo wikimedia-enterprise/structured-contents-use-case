6c62633ab1013134f1f243d82a8ea598706cf5e9
Stephan Protsack <stephan@speedandfunction.com>
2023-09-18 13:01:19 +0200

    Update the theme assignment

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { NLayout, NLayoutContent, NConfigProvider, type GlobalThemeOverrides } from 'naive-ui'
+import { NLayout, NLayoutContent, NConfigProvider, type GlobalTheme } from 'naive-ui'
+import Search  from './components/Search.vue'
 import { darkTheme } from 'naive-ui'
 
 const primaryColor = '#4263eb'
-const themeOverrides: GlobalThemeOverrides = {
+const theme: GlobalTheme = {
+  ...darkTheme,
   common: {
+    ...darkTheme.common,
     fontSize: '18px',
     primaryColor: primaryColor,
     primaryColorHover: primaryColor,
@@ -13,14 +16,16 @@ const themeOverrides: GlobalThemeOverrides = {
 </script>
 
 <template>
-  <n-config-provider :theme="darkTheme" :theme-overrides="themeOverrides">
+  <n-config-provider :theme="theme">
     <n-layout class="wme-app-container">
-      <n-layout-content></n-layout-content>
+      <n-layout-content>
+        <search/>
+      </n-layout-content>
     </n-layout>
   </n-config-provider>
 </template>
 
-<style >
+<style>
 .wme-app-container {
   min-height: 100vh !important;
 }
