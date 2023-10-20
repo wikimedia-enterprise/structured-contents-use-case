6027edfe5d980245bb98bf2b9a23561bcdee6b04
Stephan Protsack <stephan@speedandfunction.com>
2023-10-20 12:11:34 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+if [ "$1" ]; then
+  echo 'need to provide a return branch name'
+  exit 1
+else
+
+git checkout --orphan gh-pages
+
+npm run build
+
+git --work-tree dist add --all
+
+git --work-tree dist commit -m 'Deploy'
+
+git push origin HEAD:gh-pages --force
+
+rm -r dist
+
+git checkout -f "$1"
+
+git branch -D gh-pages
