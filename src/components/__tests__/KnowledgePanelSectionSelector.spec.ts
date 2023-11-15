0351bf54537a966b2c435ce7ea6840dcf78c3427
Stephan Protsack <stephan@speedandfunction.com>
2023-11-15 18:19:07 +0100

    Add mini unit test for the selector

--- src/components/__tests__/KnowledgePanelSectionSelector.spec.ts
+++ src/components/__tests__/KnowledgePanelSectionSelector.spec.ts
@@ -4,13 +4,17 @@ import KnowledgePanelSectionSelector from '../KnowledgePanelSectionSelector.vue'
 
 describe('knowledge panel section selector smoke testing', async () => {
   test('smoke test for knowledge section selector', async () => {
-    const parts = [{ name: 'Abstract', type: 'section', has_parts: [] }]
+    const parts = [
+      { name: 'Abstract', type: 'section', has_parts: [] },
+      { name: 'History', type: 'section', has_parts: [] }
+    ]
     const wrapper = mount(KnowledgePanelSectionSelector, {
       props: {
         parts
       }
     })
 
+    expect(wrapper.find('button').exists()).toBe(true)
     expect(wrapper.html()).not.empty
   })
 })
