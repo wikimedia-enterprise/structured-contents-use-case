712ad3704a91b119a33530ea50b289ec6a2ac47f
Stephan Protsack <stephan@speedandfunction.com>
2024-01-19 14:57:54 +0100

    Modify sub sections display

--- src/components/KnowledgePanelSection.vue
+++ src/components/KnowledgePanelSection.vue
@@ -22,15 +22,16 @@ const props = defineProps({
 const hasParagraphs = (part: Part) => part?.has_parts?.some(part => part.type == PartTypeParagraph)
 const isNestedSection = computed(() => (props.section as Part)?.has_parts?.some((part: Part) => hasParagraphs(part)))
 const nestedSections = computed(() => props.section?.has_parts?.filter((part: Part) => part.type == PartTypeSection))
+const expandedNames = computed(() => isNestedSection.value && !hasParagraphs(props.section as Part) ? [ nestedSections.value[0]?.name ] : [])
 </script>
 
 <template>
   <div class="wme-app-knowledge-panel-section">
     <knowledge-panel-section-text :url="url" :textSize="sectionTextSize" :section="section" />
-    <n-collapse v-if="isNestedSection">
-      <n-collapse-item v-for="section in nestedSections" :key="section.name">
+    <n-collapse v-if="isNestedSection" :default-expanded-names="expandedNames" :key="section?.name">
+      <n-collapse-item v-for="section in nestedSections" :key="section.name" :name="section.name">
         <template #header><b><i>{{ section.name }}</i></b></template>
-        <knowledge-panel-section-text :url="url" :textSize="sectionTextSize" :section="section" />
+        <knowledge-panel-section-text :url="url" :textSize="sectionTextSize" :section="section" deep-search />
       </n-collapse-item>
     </n-collapse>
   </div>
--- src/components/KnowledgePanelSectionText.vue
+++ src/components/KnowledgePanelSectionText.vue
@@ -15,21 +15,31 @@ const props = defineProps({
   textSize: {
     type: Number,
     required: true
-  }
+  },
+  deepSearch: {
+    type: Boolean,
+    required: false
+  },
 })
 
 const sectionText = computed(() => {
   const parts = props.section?.has_parts || []
-  const paragraphs = parts.map((part: Part) => part.value).join(' ')
-  const text = paragraphs.length > props.textSize ? `${paragraphs.slice(0, props.textSize - 1)}...` : paragraphs
-  return text.trim()
+  let paragraphs = parts.map((part: Part) => part.value).join(' ').trim()
+
+  if (!paragraphs && props.deepSearch) {
+    paragraphs = parts.map((part: Part) => part.has_parts?.map((part: Part) => part.value).flat()).join(' ').trim()
+  }
+
+  return paragraphs.length > props.textSize ? `${paragraphs.slice(0, props.textSize - 1)}...` : paragraphs
 })
 </script>
+
 <template>
   <p class="wme-app-knowledge-panel-section-text" v-if="sectionText">
     {{ sectionText }} <knowledge-panel-read-more :url="`${url}#${section.name?.replace(/\s/g, '_')}`" />
   </p>
 </template>
+
 <style>
 .wme-app-knowledge-panel-section-text {
   margin: 0 0 1em 0;
